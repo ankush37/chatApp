3937622ff83e294a9a80276b792d6b495a8e7b94
ankush37 <66060937+ankush37@users.noreply.github.com>
2022-12-08 18:24:50 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,119 +1,93 @@
 import socket
-from threading import Thread, Lock
-import random
+import sys
+import threading
+import os
+
+def display():
+	print("1. List all existing room")
+	print("2. Create a new chat room")
+	print("3. Join an existing room")
+
+def list_all(c):
+	c.send('ROOMS'.encode('utf8'))
+	data = c.recv(1024).decode('utf8')
+	print(data)
+
+def create_chat_room(c):
+	c.send(f'CREATE'.encode('utf8'))
+	id = int(c.recv(1024).decode('utf8'))
+	print(f"Room created with ID	{id}")
+	return id
+
+def join_room(c,id):
+	c.send("JOIN".encode('utf8'))
+	c.send(f'{id}'.encode('utf8'))
+
+def start(c, name):
+	d = c.recv(1024)
+	c.send(name.encode('utf8'))
+	while True:
+		display()
+		try:
+			ch = int(input("Enter your choice: "))
+		except Exception as e:
+			print(e)
+			sys.exit()
+		if ch==1:
+			list_all(c)
+		elif ch==2:
+			id = create_chat_room(c)
+			join_room(c,id)
+		elif ch==3:
+			id = int(input("Enter the id of room: "))
+			join_room(c,id)
+		if ch==2 or ch==3:
+			d = c.recv(1024).decode('utf8')
+			if d=='FAILURE':
+				print("Invalid id")
+			elif d=="SUCCESS":
+				os.system('cls')
+				print(f"Room joined with id {id}")
+				break
 
 
-names = {}
-rooms = {}
-user_to_room = {}
-
-lock = Lock()
-
-def create_room(num):
-	rooms[num] = []
-
-def join_room(id, c):
-	if id in rooms:
-		return True
-	return False
-
-def deal_with_client(c):
-	c.send('NICKNAME'.encode('utf8'))
-	nickname = c.recv(1024).decode('utf8')
+def listen_input(c):
 	while True:
 		data = c.recv(1024).decode('utf8')
-		if data == 'ROOMS':
-			if len(rooms.keys()) == 0:
-				c.send('No rooms available'.encode('utf8'))
-			else:
-				c.send(str(list(rooms)).encode('utf8'))
-		elif data == 'CREATE':
-			num = random.randint(1,1000)
-			while num in rooms:
-				num = random.randint(1,1000)
-				if len(rooms) >= 999:
-					break
-					sys.exit()
-			create_room(num)
-			c.send(f'{num}'.encode('utf8'))
+		if data != "":
+			print(data)
 
-		elif data == 'JOIN':
-			id = int(c.recv(1024).decode('utf8'))
-			if join_room(id,c):
-				break
-			else:
-				c.send('FAILURE'.encode('utf8'))
-
-	c.send('SUCCESS'.encode('utf8'))
-	names[c] = nickname
-	rooms[id].append(c)
-	user_to_room[c] = id
-	names[c] = nickname
-	
-	lock.acquire()
-	broadcast(f"{nickname} joined".encode('utf8'), c)
-	
-	t1 = Thread(target=threaded, args=(c,))
-	t1.daemon = True
-	t1.start()
-
-
-
-def broadcast(msg,emit):
-	clients = rooms[user_to_room[emit]]
-	for client in clients:
-
-		if client != emit:
-			client.send(msg)
-
-	lock.release()
+def take_input(c):
+	print("Please leave for exit")
+	msg = input()
+	while msg != "":
+		c.send(msg.encode('utf8'))
+		msg = input()
 
 
 def main():
-	host = ''
+	name = input("Please enter your nickname: ")
+	soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	host = "127.0.0.1"
 	port = 8000
+	try:
+		soc.connect((host, port))
+	except:
+		print("Connection Error")
+		sys.exit()
 
-	s = socket.socket()
-	s.bind((host, port))
+	start(soc, name)
+	t1 = threading.Thread(target=listen_input, args=(soc,))
+	t1.daemon = True
+	t2 = threading.Thread(target=take_input, args=(soc,))
 
-	print("Socket created")
-	s.listen(100)
+	t1.start()
+	t2.start()
 
-	print("Listening...")
-
-	while True:
-		c, addr = s.accept()
-		print(f"Connected to {addr[0]}: {addr[1]}")
-
-		t1 = Thread(target=deal_with_client, args=(c,))
-		t1.daemon = True
-		t1.start()
-
-	s.close()
-
-
-def threaded(c):
-	while True:
-		try:
-			data = c.recv(1024)
-			if data.decode('utf8')=='--QUIT--':
-				print('Bye')
-				break
-			data = f"[{names[c]}]	" + data.decode('utf8')
-			lock.acquire()
-			broadcast(data.encode('utf8'), c)
-		except Exception as e:
-			break
-
-	rooms[user_to_room[c]].remove(c)
-	del user_to_room[c]
-	lock.acquire()
-	broadcast(f"{names[c]} left".encode("utf8"),c)
-	del names[c]
-
-	c.close()
-
-if __name__ == '__main__':
-	main()
+	t2.join()
+	soc.send(b'--QUIT--')
+	sys.exit()
 
 
+if __name__ == "__main__":
+   main()
